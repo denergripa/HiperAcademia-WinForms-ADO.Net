f7eb5111eadeca01034826894113d3a49b3fd8f0
Dener Trampo <dener@hiper.com.br>
2019-11-05 22:31:36 -0200

    Exemplos de C ode firts e database firts

---
+++ EntityFrameworkDataBaseFirts/Model1.cs
@@ -0,0 +1,22 @@
+namespace EntityFrameworkDataBaseFirts
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<PedidoDeVenda> PedidoDeVenda { get; set; }
+        public virtual DbSet<PessoaFisica> PessoaFisica { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
